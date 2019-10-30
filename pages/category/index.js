fe9e2b1f84bd154f6fbcde2f425d3190635bdb8a
zhongruiqiang <q730513957@163.com>
2019-10-30 21:19:58 +0800

    分页也完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,19 +1,40 @@
 // pages/category/index.js
+import request from "../../utils/request.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    current: 0,
+    list: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
+    request({
+      url: "/api/public/v1/categories"
+    }).then(res => {
+      console.log(res.data)
+      const {
+        message
+      } = res.data
+      this.setData({
+        list: message
+      })
+    })
 
   },
 
-
+  hangdleChange(event) {
+    // console.log(event.target.dataset)
+    const {
+      index
+    } = event.target.dataset;
+    this.setData({
+      current: index
+    })
+  }
 })

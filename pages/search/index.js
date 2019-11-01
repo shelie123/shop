c3e8c7955747903f5dc3feef2d447d5175751301
zhongruiqiang <q730513957@163.com>
2019-11-01 15:26:16 +0800

    搜索静态页面已完成

---
+++ pages/search/index.js
@@ -0,0 +1,29 @@
+// pages/search/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    showValue: ""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+
+
+
+})
